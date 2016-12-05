54678bc3390cbec33b83a4fd585b6389d31b3227
Phil Barresi <me@philbarresi.com>
2016-12-05 14:43:58 -0500

    Added lecture 13 code for server and mobile app

---
+++ Lecture Code/lecture_13/more-recipes-really/src/pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'hello-ionic-page',
+  templateUrl: 'hello-ionic.html'
+})
+export class HelloIonicPage {
+  constructor() {
+
+  }
+}
