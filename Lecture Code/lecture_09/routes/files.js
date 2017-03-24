4e24b041bb3c8cb9cc387da7036c6431f2b3665b
Phil Barresi <me@philbarresi.com>
2017-03-24 00:13:14 -0400

    Added lecture 9 code

---
+++ Lecture Code/lecture_09/routes/files.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ dest: "./uploads" });
+const magick = require("../utils/magick");
+const fs = require("fs");
+const path = require("path");
+
+router.post("/", upload.single('imageToResize'), (req, res) => {
+    res.setHeader('Content-disposition', 'attachment; filename=result.png');
+    res.setHeader('Content-type', "image/png");
+
+    const fileStream = fs.createReadStream(path.resolve(req.file.path));
+    const resultStream = magick.makeThumbnail(fileStream);
+
+    resultStream.pipe(res);
+});
+
+module.exports = router;
