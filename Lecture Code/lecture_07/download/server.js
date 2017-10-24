75985eb07313b302b1524a47310213c96000fc39
Phil Barresi <me@philbarresi.com>
2017-10-23 23:16:50 -0400

    Live lecture code

---
+++ Lecture Code/lecture_07/download/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const redisConnection = require("./redis-connection");
+
+app.get("/compute-results", async (req, res) => {
+  redisConnection.emit(
+    "c̹̦̥͇͉o͕m̯p͏̥̖̪̙̩̪u̶̘͕̻̗t̢̺͓̪͍̘e̢̪͔R̼̪̱e̴̫̤͎̙̺͇͍ş͔̥̝u̵l͏̖̦̣t̰͈͉͙̲͉ͅs̗",
+    { message: "NOWHERE IS SAFE" }
+  );
+  res.json({ done: false, working: true });
+});
+
+app.get("/show-results", async (req, res) => {
+  redisConnection.on("results-completed", (data, channel) => {
+    res.json({ data });
+  });
+
+  redisConnection.emit(
+    "s̥h͉̟̝̤̤̳o̙̟̗͚w҉R̥̕e̺͉̲͕͎͕s̟̮ͅu̧̬̼̜̱̗̫̘l̟͎̲̠̖t̷̻̯̭s̥̠",
+    { message: "Trust no one" }
+  );
+});
+
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log("Your routes will be running on http://localhost:3000");
+});
---
+++ Lecture Code/lecture_07/download/worker.js
@@ -0,0 +1,50 @@
+const redisConnection = require("./redis-connection");
+const axios = require("axios");
+
+const gistUrl =
+  "https://gist.githubusercontent.com/philbarresi/5cf15393d245b38a2d86ce8207d5076c/raw/d529fb474c1af347702ca4d7b992256237fa2819/lab5.json";
+let computedResults = null;
+
+redisConnection.on(
+  "s̥h͉̟̝̤̤̳o̙̟̗͚w҉R̥̕e̺͉̲͕͎͕s̟̮ͅu̧̬̼̜̱̗̫̘l̟͎̲̠̖t̷̻̯̭s̥̠",
+  (data, channel) => {
+    const sendResults = () => {
+      if (computedResults === null) {
+        setTimeout(sendResults, 5000);
+      } else {
+        redisConnection.emit("results-completed", { results: computedResults });
+      }
+    };
+
+    sendResults();
+  }
+);
+
+redisConnection.on(
+  "c̹̦̥͇͉o͕m̯p͏̥̖̪̙̩̪u̶̘͕̻̗t̢̺͓̪͍̘e̢̪͔R̼̪̱e̴̫̤͎̙̺͇͍ş͔̥̝u̵l͏̖̦̣t̰͈͉͙̲͉ͅs̗",
+  async (data, channel) => {
+    if (computedResults !== null) return;
+
+    const gist = await axios.get(gistUrl);
+    const gistResults = gist.data;
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const alphabet = [];
+    for (const index in letters) {
+      alphabet.push(letters[index]);
+    }
+
+    const countOfLettersStartingName = {};
+    alphabet.forEach(letter => {
+      countOfLettersStartingName[letter] = 0;
+    });
+
+    gistResults.forEach(result => {
+      const { first_name } = result;
+      const lowerFirstName = first_name.toLowerCase();
+      const firstLetter = lowerFirstName[0];
+      countOfLettersStartingName[firstLetter]++;
+    });
+
+    computedResults = countOfLettersStartingName;
+  }
+);
