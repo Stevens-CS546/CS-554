ace4a8450871169f402a8f1131a92e28971c8eb1
Phil Barresi <me@philbarresi.com>
2017-11-10 13:57:24 -0500

    Updates

--- Lecture Code/lecture_08/chat/app.js
+++ Lecture Code/lecture_08/chat/app.js
@@ -1,39 +1,42 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+
+const io = require("socket.io")(http);
+
 const chat = io.of("/chat");
 const usersToSocket = {};
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-chat.on('connection', (socket) => {
-  socket.on('join-room', (data) => {
+chat.on("connection", socket => {
+  socket.on("join-room", data => {
     socket.leave(data.previousRoom);
     socket.join(data.newRoom);
 
     socket.emit("joined-room", data.newRoom);
   });
 
-  socket.on('direct message', (msg) => {
-    usersToSocket[msg.userName].emit('private message', {
+  socket.on("direct message", msg => {
+    usersToSocket[msg.userName].emit("private message", {
       from: msg.fromUserName,
       text: msg.text
     });
   });
 
-  socket.on('setup', (connectionInfo) => {
+  socket.on("setup", connectionInfo => {
     usersToSocket[connectionInfo.nickname] = socket;
   });
 
-  socket.on('send-message', (msg) => {
-    chat.to(msg.room).emit('receive-message', msg.text);
+  socket.on("send-message", msg => {
+    chat.to(msg.room).emit("receive-message", msg.text);
   });
 
-  socket.emit('request-credentials');
+  socket.emit("request-credentials");
 });
 
 http.listen(3000, () => {
-  console.log('listening on http://localhost:3000');
+  console.log("listening on http://localhost:3000");
 });
