506afaf853467d09305403ede6bae7024905ee05
Phil Barresi <me@philbarresi.com>
2017-11-10 15:34:12 -0500

    Added ls example

--- Lecture Code/lecture_09/routes/files.js
+++ Lecture Code/lecture_09/routes/files.js
@@ -1,22 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const multer = require('multer');
+const multer = require("multer");
 const upload = multer({ dest: "./uploads" });
 
 const magick = require("../utils/magick");
+const ls = require("../utils/ls");
 
 const fs = require("fs");
 const path = require("path");
 
-router.post("/", upload.single('imageToResize'), (req, res) => {
-    res.setHeader('Content-disposition', 'attachment; filename=result.png');
-    res.setHeader('Content-type', "image/png");
+router.get("/listing", async (req, res) => {
+  const resultData = await ls.listDirectory(req.query.folder);
+  res.status(200).send(resultData);
+});
 
-    const fileStream = fs.createReadStream(path.resolve(req.file.path));
-    const resultStream = magick.makeThumbnail(fileStream);
+router.post("/", upload.single("imageToResize"), (req, res) => {
+  res.setHeader("Content-disposition", "attachment; filename=result.png");
+  res.setHeader("Content-type", "image/png");
 
-    resultStream.pipe(res);
+  const fileStream = fs.createReadStream(path.resolve(req.file.path));
+  const resultStream = magick.makeThumbnail(fileStream);
+
+  resultStream.pipe(res);
 });
 
 module.exports = router;
---
+++ Lecture Code/lecture_09/utils/ls.js
@@ -0,0 +1,45 @@
+const spawn = require("child_process").spawn;
+function uint8arrayToStringMethod(myUint8Arr){
+  return String.fromCharCode.apply(null, myUint8Arr);
+}
+
+/**
+ * crops and resizes images to our desired size
+ * @param {Stream} streamIn in stream containing the raw image
+ * @return {Stream}
+ */
+exports.listDirectory = filePath => {
+  return new Promise((resolver, rejector) => {
+    const command = "ls";
+    const args = ["-la", filePath];
+
+    const proc = spawn(command, args);
+
+    let gatheredByteArray = new Uint8Array(0);
+    
+    proc.stderr.on("data", chunk => {
+      // stream.emit("error", chunk);
+    });
+
+    proc.stdout.on("data", chunk => {
+      const newByteArray = new Uint8Array(gatheredByteArray.length + chunk.length);
+
+      newByteArray.set(gatheredByteArray);
+      newByteArray.set(chunk, gatheredByteArray.length);
+      gatheredByteArray = newByteArray;
+      //stream.emit("data", chunk);
+    });
+
+    proc.stdout.on("end", chunk => {
+      const body = uint8arrayToStringMethod(gatheredByteArray);
+
+      resolver(body);
+    });
+
+    proc.on("error", chunk => {
+      // stream.emit("error", chunk);
+    });
+  });
+
+  return stream;
+};
